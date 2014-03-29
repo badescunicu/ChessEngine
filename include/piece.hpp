1f16ba24f3f921326806b1d78c6dfcb90e4d978b
Nicu Badescu <badescunicu@yahoo.com>
2014-03-29 23:51:14 +0200

    Use enum instead of define

--- include/piece.hpp
+++ include/piece.hpp
@@ -1,19 +1,18 @@
 #ifndef __PIECE__HPP
 #define __PIECE__HPP
 
-// Define pieces bit representation
-#define NONE 0
-#define PAWN_W 1
-#define KNIGHT_W 2
-#define BISHOP_W 3
-#define ROOK_W 4
-#define QUEEN_W 5
-#define KING_W 6
-#define PAWN_B 9
-#define KNIGHT_B 10
-#define BISHOP_B 11
-#define ROOK_B 12
-#define QUEEN_B 13
-#define KING_B 14
-
-#endif
+enum PieceType {
+    NONE = 0,
+    PAWN_W, 
+    KNIGHT_W, 
+    BISHOP_W, 
+    ROOK_W, 
+    QUEEN_W, 
+    KING_W, 
+    PAWN_B = 9, 
+    KNIGHT_B, 
+    BISHOP_B, 
+    ROOK_B, 
+    QUEEN_B, 
+    KING_B 
+};
