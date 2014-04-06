a9d48f47beeb757aa356e42bdf181cd8b6b312b7
Stefan Mirea <mireastefangabriel@gmail.com>
2014-04-06 14:23:06 +0300

    Added command line arguments so that two running instances can log to different files

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,18 @@
 #include <fstream>
 #include <game.hpp>
 
-enum Mode { SENT, RECEIVED };
-
-void input_output(Logger& logger, Mode direction, std::string text) {
-    logger.log(std::string(direction == SENT ? "Sent:     " : "Received: ")
-               + text);
-    if (direction == SENT)
-        std::cout << text << "\n";
+void input(Logger& logger, std::string& text) {
+    std::getline(std::cin, text);
+    logger.log(std::string("Received: ") + text);
 }
 
-void print_status(Logger& logger, bool force, Color my_color, Color on_move)
-{
+void output(Logger& logger, const std::string text) {
+    std::cout << text << "\n";
+    logger.log(std::string("Sent:     ") + text);
+}
+
+void print_status(Logger& logger, const bool force, const Color my_color,
+                  const Color on_move) {
     logger.log(std::string("Status:   I'm ")
                + (my_color == WHITE ? " WHITE" :
                  (my_color == BLACK ? " BLACK" : "NOBODY"))
@@ -23,12 +24,12 @@ void print_status(Logger& logger, bool force, Color my_color, Color on_move)
                + (force ? " On." : "Off."));
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     bool force = false;
     Color last_color_received = NO_COLOR;
     std::string x_command, last_move_received;
     Game *g = NULL;
-    Logger logger("low_level_log.txt");
+    Logger logger(argc > 1 ? argv[1] : "low_level_log.txt");
 
     srand(time(NULL));
 
@@ -37,10 +38,9 @@ int main() {
     std::cout.rdbuf()->pubsetbuf(0, 0);
 
     while (1) {
-        std::getline(std::cin, x_command);
-        input_output(logger, RECEIVED, x_command);
+        input(logger, x_command);
         if (x_command == "xboard") {
-            input_output(logger, SENT, "");
+            output(logger, "");
         } else if (x_command == "new") {
             force = false;
             last_move_received = std::string();
@@ -58,21 +58,21 @@ int main() {
             if (g->get_color_on_move() == NO_COLOR)
                 g->set_color_on_move(last_color_received);
             g->set_color(g->get_color_on_move());
-            input_output(logger, SENT, g->send_best_move());
+            output(logger, g->send_best_move());
         } else if (x_command.find("usermove") == 0) {
             last_move_received = x_command.substr(9);
             if (!g->get_move(last_move_received))
-                input_output(logger, SENT, "Illegal move: " + last_move_received);
+                output(logger, "Illegal move: " + last_move_received);
             else if (!force)
-                input_output(logger, SENT, g->send_best_move());
+                output(logger, g->send_best_move());
             else {
                 std::string game_result;
                 if (!g->get_all_moves(game_result).size())
-                    input_output(logger, SENT, game_result);
+                    output(logger, game_result);
             }
         } else if (x_command.find("protover") != std::string::npos) {
-            input_output(logger, SENT, "feature done=1 sigint=0 sigterm=0 usermove=1");
-            input_output(logger, SENT, "feature myname=\"Carlsen's Apprentices\"");
+            output(logger, "feature done=1 sigint=0 sigterm=0 usermove=1");
+            output(logger, "feature myname=\"Carlsen's Apprentices\"");
         } else if (x_command == "white") {
             last_color_received = WHITE;
         } else if (x_command == "black") {
@@ -82,16 +82,16 @@ int main() {
                 delete g;
             g = new Game(NO_COLOR, NO_COLOR, CUSTOM);
             last_move_received = std::string();
-            std::getline(std::cin, x_command); // skipping '#'
-            std::getline(std::cin, x_command);
+            input(logger, x_command); // skipping '#'
+            input(logger, x_command);
             while (x_command != "c") {
                 g->add_piece(x_command, WHITE);
-                std::getline(std::cin, x_command);
+                input(logger, x_command);
             }
-            std::getline(std::cin, x_command);
+            input(logger, x_command);
             while (x_command != ".") {
                 g->add_piece(x_command, BLACK);
-                std::getline(std::cin, x_command);
+                input(logger, x_command);
             }
             g->guess_castlings();
         }
